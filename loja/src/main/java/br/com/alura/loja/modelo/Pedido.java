c5ec9cbc08198b820ee7bab418c06f5d20f5ff92
renanbevi <renanbevi@gmail.com>
2023-01-07 16:32:59 -0300

    Criação de mais 2 entidades Cliente e Pedido

---
+++ loja/src/main/java/br/com/alura/loja/modelo/Pedido.java
@@ -0,0 +1,63 @@
+package br.com.alura.loja.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "pedidos")
+
+public class Pedido {
+
+	@ManyToOne
+	private Cliente cliente;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private LocalDate data = LocalDate.now();
+	private BigDecimal valortotal;
+
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public BigDecimal getValortotal() {
+		return valortotal;
+	}
+
+	public void setValortotal(BigDecimal valortotal) {
+		this.valortotal = valortotal;
+	}
+
+}
